49cfd9776f5061c5bc79db8ab0cd4e51f24878f1
Kurt Weber <weberkrt@msn.com>
2016-10-05 21:46:20 -0400

    MS MVC Video Project

---
+++ MvcMusicStore/Models/Album.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStore.Models
+{
+    public class Album
+    {
+        public int AlbumID { get; set; }
+
+        public string Title { get; set; }
+
+        public Artist Artist { get; set; }
+    }
+}
